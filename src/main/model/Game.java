31341d758662ce2d3b7b4342db1432ec59c09681
“bradeu” <bradeu01@gmail.com>
2024-02-12 20:21:18 -0800

    update game class

--- src/main/model/Game.java
+++ src/main/model/Game.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Game {
 
@@ -14,13 +15,26 @@ public class Game {
         teamList.add(team);
     }
 
+    public void simGame(Team winTeam) {
+        Random random = new Random();
+        int winScore = 0;
+        int loseScore = 0;
+        while (winScore < 140) {
+            winScore += random.nextInt(10);
+            loseScore += random.nextInt(10);
+            System.out.println(winScore - loseScore);
+        }
+        System.out.println(winTeam.getName() + "wins the match !");
+    }
+
     public String playGame() {
-        Team winner = new Team("no name");
+        Team winner = new Team("no winner");
         for (Team team : teamList) {
             if (team.getRating() > winner.getRating()) {
+                simGame(team);
                 winner = team;
             }
         }
-        return winner.getName();
+        return winner.getName() + "wins the game !";
     }
 }
