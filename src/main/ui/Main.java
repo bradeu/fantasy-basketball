0c5352988acbc17a9dad80929f09119ea5986aee
“bradeu” <bradeu01@gmail.com>
2024-02-14 22:16:01 -0800

    update main class

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,11 +1,32 @@
 package ui;
 
 import model.Game;
+import model.Team;
+import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         Game game = new Game();
 
+        System.out.println("Enter number of teams:");
+        int teamAmt = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int index = 0; index < teamAmt; index++) {
+            System.out.println("Enter team name:");
+            String teamName = scanner.nextLine();
+
+            Team team = new Team(teamName);
+
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Enter player name:");
+                String playerName = scanner.nextLine();
+            }
+        }
+        game.playGame();
     }
 }
